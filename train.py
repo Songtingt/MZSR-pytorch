8c0e58462f9221a7898e28b93f7e7f779d6c2aae
Potatogood <53066527+Songtingt@users.noreply.github.com>
2021-08-16 10:26:51 +0800

    Update train.py
    modify the MAML part

--- train.py
+++ train.py
@@ -33,7 +33,7 @@ class Train(object):
         self.META_ITER = meta_iter
 
         self.TASK_BATCH_SIZE = task_batch_size
-        self.TASK_LR = task_lr
+        self.TASK_LR = task_lr  #1e-2
         self.TASK_ITER = task_iter
 
         # self.data_generator=data_generator
@@ -42,6 +42,10 @@ class Train(object):
         self.work_dir=args.work_dir  #./experiments
         self.use_weighted_loss=args.use_weighted_loss
         print('use weighted loss?',self.use_weighted_loss)
+        if self.use_weighted_loss:
+            self.LW = self.get_loss_weights()
+        else: 
+            self.LW=torch.ones((self.TASK_ITER))
 
         timestamp = time.strftime('%Y%m%d_%H%M%S', time.localtime())
         self.work_dir = os.path.join(self.work_dir, timestamp)  # 创建work_dir
@@ -85,12 +89,14 @@ class Train(object):
             #     new_state_dict[name] = v  # 新字典的key值对应的value为一一对应的值。
             # self.model.load_state_dict(self.pretrain_weights)
             # self.logger.info('load checkpoint from %s', self.loadfrom_dir)
+
             self.vars=nn.ParameterList()
             for name, param in self.pretrain_weights.items():  #装着可以放进Model的权重
                 print(name, param)
                 self.vars.append(nn.Parameter(param))
             self.model.vars=self.vars #将预训练的参数放入
             print(self.model.vars[15])  #看一下是否Load成功 
+
 
 
         param_nums = 0
@@ -111,7 +117,7 @@ class Train(object):
         '''Optimizers'''
         self.pretrain_op = optim.Adam(self.model.parameters(), lr=self.META_LR)
         self.opt = optim.Adam(self.model.parameters(), lr=self.META_LR)
-        print(self.model.module.parameters()[15])
+        # print(self.model.module.parameters())
 
     def construct_model(self, inf):
         def task_meta_learning(inputa, labela, inputb, labelb):
@@ -122,17 +128,18 @@ class Train(object):
             inputb = torch.as_tensor(inputb).type(torch.FloatTensor).to(self.device).permute(0, 3, 1, 2)  # 8,3,64,64
             labelb = torch.as_tensor(labelb).type(torch.FloatTensor).to(self.device).permute(0, 3, 1, 2)
 
-            task_outputbs, task_lossesb = [], []
+            task_outputbs= []
+            task_lossesb=0
             
             # self.model.load_state_dict(self.pretrain_weights)  #每个任务的起点都一样，都用的是加载进来的参数作为权重
             task_outputa = self.model(inputa,vars=None)  #support set
-            print('see var[15]',self.model.module.vars[15])  # 看一下每一轮训练的时候，参数是否一样
+            # print('see var[15]',self.model.module.vars[15])  # 看一下每一轮训练的时候，参数是否一样
 
             task_lossa = self.loss_fn(labela, task_outputa)
             grad = torch.autograd.grad(task_lossa, self.model.parameters())
-            print(grad)
+            # print(grad)
             fast_weights = list(map(lambda p: p[1] - self.TASK_LR * p[0], zip(grad, self.model.parameters())))
-            print(fast_weights[0])
+            # print(fast_weights[0])
 
             '''
             # this is the loss before first update use the model's original parameters
@@ -146,7 +153,8 @@ class Train(object):
             with torch.no_grad():
                 output = self.model(inputb, fast_weights)
                 task_outputbs.append(output)  # 存放test的输出
-                task_lossesb.append(self.loss_fn(labelb, output))  # 先把task_lossb都放到列表中 最后再来更新meta_learner的梯度
+                # task_lossesb.append(self.loss_fn(labelb, output))  # 先把task_lossb都放到列表中 最后再来更新meta_learner的梯度
+                task_lossesb+=self.loss_fn(labelb, output)*self.LW[0]
                 
                 
 
@@ -159,39 +167,37 @@ class Train(object):
 
                 output = self.model(inputb,fast_weights)  # inputb是test
                 task_outputbs.append(output)  # 存放test的输出
-                task_lossesb.append(self.loss_fn(labelb, output))  # 先把task_lossb都放到列表中 最后再来更新meta_learner的梯度
+                # task_lossesb.append(self.loss_fn(labelb, output))  # 先把task_lossb都放到列表中 最后再来更新meta_learner的梯度
+                task_lossesb+= self.loss_fn(labelb, output)*self.LW[i+1]  #1~4
                 # print('task_lossesb',task_lossesb[-1].requires_grad) TRUE
                 #task_lossesb 中总共存放了 TASK_ITER个loss,每个Loss都是一个Meta batch的数据的Loss之和
                 # print(task_lossesb[-1]) #看看是不是对batchsize=8个数据求了均值后的数据，是的，是一个数
             # print("len(task_lossesb)",len(task_lossesb))  #=5
+            task_lossesb/=self.TASK_ITER
             
             task_output = [task_outputa, task_outputbs, task_lossa, task_lossesb]
 
             return task_output
 
         self.total_lossa = 0.
-        self.total_lossesb = []
+        self.total_lossesb = 0.
         inputa, labela, inputb, labelb = inf
         for i in range(self.META_BATCH_SIZE):  #5
             res = task_meta_learning(inputa[i], labela[i], inputb[i], labelb[i])  # inputa[i] (8, 64, 64, 3)
             # print(res[2])
             self.total_lossa += res[2]  #先求和，然后再除以meta batch size
-            self.total_lossesb.append(res[3])  #5个iter loss的均值
+            # self.total_lossesb.append(res[3][-1])  #5个iter loss的均值
+            self.total_lossesb+= res[3] #5个iter loss的均值
             # self.total_lossesb.append(sum(res[3]) / self.TASK_ITER)  #5个iter loss的均值
         self.total_lossa /= self.META_BATCH_SIZE  # 训练的Loss
 
         # print("len(self.total_lossesb)", len(self.total_lossesb))  #理论上等于META_BATCH_SIZE
-        self.total_lossesb=torch.tensor(self.total_lossesb,requires_grad=True)  #5,5 torch.tensor 默认为false
-        self.total_lossesb=torch.mean(self.total_lossesb,dim=0)  #相当于除以META_BATCH_SIZE  torch.Size([5])
-        if self.use_weighted_loss:
-            LW = self.get_loss_weights()
-            self.weighted_total_lossesb = torch.mean(self.total_lossesb * LW)
-            self.total_lossesb=torch.mean(self.total_lossesb)
-            print(self.weighted_total_lossesb, LW)
-        else:
-            self.weighted_total_lossesb = torch.mean(self.total_lossesb)
-            self.total_lossesb = self.weighted_total_lossesb
-            print(self.weighted_total_lossesb)
+        # self.total_lossesb=torch.tensor(self.total_lossesb,requires_grad=True)  #5,5 torch.tensor 默认为false
+        # self.total_lossesb=torch.mean(self.total_lossesb,dim=0)  #相当于除以META_BATCH_SIZE  torch.Size([5])
+        self.total_lossesb/= self.META_BATCH_SIZE#相当于除以META_BATCH_SIZE  torch.Size([5])
+
+        print(self.total_lossesb)
+
 
 
         # print(self.weighted_total_lossesb.requires_grad)  #为啥还是false????
@@ -199,10 +205,10 @@ class Train(object):
 
         # print(self.weighted_total_lossesb.requires_grad)
         self.opt.zero_grad()
-        self.weighted_total_lossesb.backward()
+        self.total_lossesb.backward()
         self.opt.step()  # 该优化器的学习率是meta lr =1e-4 β
-        print(self.model.module.parameters()[15])
-        print(self.model.module.vars[15])
+        # print(self.model.module.parameters()[15])
+        # print(self.model.module.vars[15])
 
     def get_loss_weights(self):
         
@@ -245,7 +251,8 @@ class Train(object):
             if step % PRINT_ITER == 0:
                 t1 = t2
                 t2 = time.time()
-                self.logger.info('Iter: %d - Pre loss %.3f - Post weighted loss: %.3f - Post loss: %.3f - Time: %.2f'%(step,self.total_lossa,self.weighted_total_lossesb,self.total_lossesb,t2 - t1))
+                # self.logger.info('Iter: %d - Pre loss %.3f - Post weighted loss: %.3f - Post loss: %.3f - Time: %.2f'%(step,self.total_lossa,self.weighted_total_lossesb,self.total_lossesb,t2 - t1))
+                self.logger.info('Iter: %d - Pre loss %.3f - Post loss: %.3f - Time: %.2f'%(step,self.total_lossa,self.total_lossesb,t2 - t1))
 
 
             if step % SAVE_ITET == 0:
